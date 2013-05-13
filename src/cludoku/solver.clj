bec2560fba421c799d470dbc668bfe840ad49771
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2013-05-14 01:03:40 +0200

    Implementation of a possible rule function

--- src/cludoku/solver.clj
+++ src/cludoku/solver.clj
@@ -14,3 +14,19 @@
   (conj board
         {:cells (map fill-in-last-digits
                      (:cells board))}))
+
+(defn ^:export remove-doubles [cell-set]
+  (let [possible-repeated-pair
+        (first (first (filter #(= (nth % 1) 2)
+                              (frequencies ((group-by #(count %)
+                                                      (vals cell-set)) 2)))))]
+    (if possible-repeated-pair
+      (into {} (map (fn [cell-pair] [(first cell-pair)
+                                    (if (= (second cell-pair)
+                                           possible-repeated-pair)
+                                      (second cell-pair)
+                                      (into #{} (remove possible-repeated-pair
+                                                        (second cell-pair))))])
+                    (filter #(and (some possible-repeated-pair (second %))
+                                  (not= possible-repeated-pair (second %))) cell-set)))
+      {})))
--- test/cludoku/solver_test.clj
+++ test/cludoku/solver_test.clj
@@ -15,4 +15,20 @@
             :cells [[1   4 3   2]
                     [2   3 nil nil]
                     [3   2 nil nil]
-                    [4   1 nil nil]]}))))
+                    [4   1 nil nil]]})))
+
+  (testing "Can remove doubles from a set of cells"
+    (is (= (remove-doubles {[0 0] #{2 4}, [0 1] #{1 2 3 4 5},
+                            [0 2] #{2 4}, [0 3] #{7 8 9},
+                            [0 4] #{1 3 5}, [0 5] #{6 8},
+                            [0 6] #{5 7 9}, [0 7] #{3 4 5 6},
+                            [0 8] #{3 5 6 7}})
+           {[0 1] #{1 3 5} [0 7] #{3 5 6}})))
+
+  (testing "Doesn't try to remove doubles if there are no real doubles"
+    (is (= (remove-doubles {[0 0] #{2 4}, [0 1] #{1 2 3 4 5},
+                            [0 2] #{2 4 5}, [0 3] #{7 8 9},
+                            [0 4] #{1 3 5}, [0 5] #{6 8},
+                            [0 6] #{5 7 9}, [0 7] #{3 4 5 6},
+                            [0 8] #{3 5 6 7}})
+           {}))))
