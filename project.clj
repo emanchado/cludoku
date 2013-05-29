fb9cf12c67c36e7d5f49e16c4e14bf7621b89667
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2013-05-29 22:12:46 +0200

    Update project.clj real description, URL and version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject cludoku "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject cludoku "0.0.1-SNAPSHOT"
+  :description "Simple Sudoku solver"
+  :url "http://github.com/emanchado/cludoku"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
