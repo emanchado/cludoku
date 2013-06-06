caaf53a92408198a780434607c0ffb09f61e341b
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2013-06-07 00:18:15 +0200

    Drop the solve-step function, focus on rules for now

--- src/cludoku/solver.clj
+++ src/cludoku/solver.clj
@@ -2,31 +2,19 @@
   (:use clojure.set)
   (:use cludoku.board))
 
-(defn fill-in-last-digits [cell-set]
+(defn remove-final-numbers [cell-set]
   (let [existing-numbers (set (map first (filter #(= (count %) 1)
-                                                 (map second cell-set))))
-        possible-numbers (set (range 1 (inc (count cell-set))))]
-    (if (= (count existing-numbers)
-           (dec (count cell-set)))
-      (let [missing-number (first (clojure.set/difference possible-numbers
-                                                          existing-numbers))
-            unsolved-cell-coords (ffirst (filter #(not= (count (second %)) 1)
-                                                 cell-set))]
-        [unsolved-cell-coords #{missing-number}]))))
+                                                 (map second cell-set))))]
+    (into {} (map (fn [cell]
+                    [(first cell)
+                     (set (remove existing-numbers (second cell)))])
+                  (filter (fn [cell]
+                            (and (some #(contains? (second cell) %)
+                                       existing-numbers)
+                                 (> (count (second cell)) 1)))
+                          cell-set)))))
 
-(defn ^:export solve-step [board]
-  (let [updates (reduce
-                 (fn [acc number]
-                   (merge acc
-                          (fill-in-last-digits (board-row board number))
-                          (fill-in-last-digits (board-col board number))
-                          (fill-in-last-digits (board-block board number))))
-                 {}
-                 (range (dim board)))]
-    (conj board
-          {:cells (merge (:cells board) updates)})))
-
-(defn ^:export remove-doubles [cell-set]
+(defn remove-doubles [cell-set]
   (let [possible-repeated-pair
         (ffirst (filter #(= (nth % 1) 2)
                         (frequencies ((group-by #(count %)
--- test/cludoku/solver_test.clj
+++ test/cludoku/solver_test.clj
@@ -4,61 +4,13 @@
         cludoku.solver))
 
 (deftest solver
-  (testing "Can fill in the last digit left in a row"
-    (is (= (solve-step (create-board {:block-height 2
-                                      :block-width 2
-                                      :cells [[1   4 3   nil]
-                                              [2   3 nil nil]
-                                              [3   2 nil nil]
-                                              [4   1 nil nil]]}))
-           (create-board {:block-height 2
-                          :block-width 2
-                          :cells [[1   4 3   2]
-                                  [2   3 nil nil]
-                                  [3   2 nil nil]
-                                  [4   1 nil nil]]}))))
-
-  (testing "Can fill in the last digit left in a row (last digit)"
-    (is (= (solve-step (create-board {:block-height 2
-                                      :block-width 2
-                                      :cells [[1   2 3   nil]
-                                              [4   3 nil nil]
-                                              [3   4 nil nil]
-                                              [2   1 nil nil]]}))
-           (create-board {:block-height 2
-                          :block-width 2
-                          :cells [[1   2 3   4]
-                                  [4   3 nil nil]
-                                  [3   4 nil nil]
-                                  [2   1 nil nil]]}))))
-
-  (testing "Can fill in the last digit left in a column"
-    (is (= (solve-step (create-board {:block-height 2
-                                      :block-width 2
-                                      :cells [[1   4   3   2]
-                                              [2   3   nil nil]
-                                              [3   nil nil nil]
-                                              [4   1   nil nil]]}))
-           (create-board {:block-height 2
-                          :block-width 2
-                          :cells [[1   4 3   2]
-                                  [2   3 nil nil]
-                                  [3   2 nil nil]
-                                  [4   1 nil nil]]}))))
-
-  (testing "Can fill in the last digit left in a block"
-    (is (= (solve-step (create-board {:block-height 2
-                                      :block-width 2
-                                      :cells [[nil 4   nil   2]
-                                              [nil nil nil nil]
-                                              [3   nil nil nil]
-                                              [4   1   nil nil]]}))
-           (create-board {:block-height 2
-                          :block-width 2
-                          :cells [[nil 4   nil   2]
-                                  [nil nil nil nil]
-                                  [3   2   nil nil]
-                                  [4   1   nil nil]]}))))
+  (testing "Can remove final numbers from candidates in a set of cells"
+    (is (= (remove-final-numbers {[0 0] #{2}, [0 1] #{1 2 3 4 5 7},
+                                  [0 2] #{2 4}, [0 3] #{7},
+                                  [0 4] #{1 3 5}, [0 5] #{6 8},
+                                  [0 6] #{5 7 9}, [0 7] #{3 4 5 6},
+                                  [0 8] #{3 5 6 7}})
+           {[0 1] #{1 3 4 5} [0 2] #{4} [0 6] #{5 9} [0 8] #{3 5 6}})))
 
   (testing "Can remove doubles from a set of cells"
     (is (= (remove-doubles {[0 0] #{2 4}, [0 1] #{1 2 3 4 5},
@@ -66,12 +18,12 @@
                             [0 4] #{1 3 5}, [0 5] #{6 8},
                             [0 6] #{5 7 9}, [0 7] #{3 4 5 6},
                             [0 8] #{3 5 6 7}})
-           {[0 1] #{1 3 5} [0 7] #{3 5 6}})))
+           {[0 1] #{1 3 5} [0 7] #{3 5 6}}))))
 
-  (testing "Doesn't try to remove doubles if there are no real doubles"
-    (is (= (remove-doubles {[0 0] #{2 4}, [0 1] #{1 2 3 4 5},
-                            [0 2] #{2 4 5}, [0 3] #{7 8 9},
-                            [0 4] #{1 3 5}, [0 5] #{6 8},
-                            [0 6] #{5 7 9}, [0 7] #{3 4 5 6},
-                            [0 8] #{3 5 6 7}})
-           {}))))
+(testing "Doesn't try to remove doubles if there are no real doubles"
+  (is (= (remove-doubles {[0 0] #{2 4}, [0 1] #{1 2 3 4 5},
+                          [0 2] #{2 4 5}, [0 3] #{7 8 9},
+                          [0 4] #{1 3 5}, [0 5] #{6 8},
+                          [0 6] #{5 7 9}, [0 7] #{3 4 5 6},
+                          [0 8] #{3 5 6 7}})
+         {})))
