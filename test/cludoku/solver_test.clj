a16d205888b5a8632323298b2f109c9fc85abb15
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2013-06-20 01:32:07 +0200

    Trivial formatting fixes

--- test/cludoku/solver_test.clj
+++ test/cludoku/solver_test.clj
@@ -19,6 +19,7 @@
                             [0 6] #{5 7 9}, [0 7] #{3 4 5 6},
                             [0 8] #{3 5 6 7}})
            {[0 1] #{1 3 5} [0 7] #{3 5 6}})))
+
   (testing "Doesn't try to remove doubles if there are no real doubles"
     (is (= (remove-doubles {[0 0] #{2 4}, [0 1] #{1 2 3 4 5},
                             [0 2] #{2 4 5}, [0 3] #{7 8 9},
@@ -26,6 +27,7 @@
                             [0 6] #{5 7 9}, [0 7] #{3 4 5 6},
                             [0 8] #{3 5 6 7}})
            {})))
+
   (testing "A candidate is chosen when it only appears in one cell"
     (is (= (single-cell-candidate {[0 0] #{3 4} [0 1] #{3 4 9} [0 2] #{8}
                                    [1 0] #{2 4 5} [1 1] #{4 5 9} [1 2] #{5 9}
