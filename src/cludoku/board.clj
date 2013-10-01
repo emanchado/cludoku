d38a171b954d6e4c1b59c13aaa4f2c38fa6368e2
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2013-10-01 23:30:19 +0200

    Documentation functions in board.clj

--- src/cludoku/board.clj
+++ src/cludoku/board.clj
@@ -2,15 +2,22 @@
 (require '[comb.template :as template])
 
 (defn dim [board]
+  "Returns the board side size, ie. the number of different symbols to
+   be used in the board."
   (* (:block-height board) (:block-width board)))
 
 (defn board-row [board row-number]
+  "Returns a cell set with the row identified by the given row
+   number."
   (merge {} (filter #(= (first (first %)) row-number) (:cells board))))
 
 (defn board-col [board col-number]
+  "Returns a cell set with the column identified by the given column
+   number."
   (merge {} (filter #(= (second (first %)) col-number) (:cells board))))
 
 (defn board-block [board block-number]
+  "Returns a cell set with the block identified by the given block number."
   (let [first-row (* (quot block-number (:block-height board))
                      (:block-height board))
         last-row  (dec (+ first-row (:block-height board)))
@@ -24,12 +31,18 @@
                       (:cells board)))))
 
 (defn block-for-cell [board [row-n col-n]]
+  "Returns a cell set with the entire block that contains the given
+   set of coordinates."
   (let [block-row (quot row-n (:block-height board))
         block-col (quot col-n (:block-width board))
         block-number (+ (* (:block-height board) block-row) block-col)]
     (board-block board block-number)))
 
 (defn cell-set-diff [cell-set1 cell-set2]
+  "Returns a map of differences between two cell sets where keys are
+   coordinates that had different candidates in both cell sets, and
+   values are two-element vectors with the candidates for each cell
+   set. Used to get better error messages in tests."
   (reduce (fn [acc coord]
             (let [cands1 (get cell-set1 coord)
                   cands2 (get cell-set2 coord)]
@@ -40,6 +53,9 @@
           (keys cell-set1)))
 
 (defn well-formed? [proto-board]
+  "Returns true if the given proto-board is properly formed, meaning
+   it has the right number of rows and columns, and all cells contain
+   either nil or a number in the appropriate range."
   (let [dimensions (* (:block-height proto-board) (:block-width proto-board))]
     (and (every? #(= (count %) dimensions) (:cells proto-board))
          (= (count (:cells proto-board)) dimensions)
@@ -50,6 +66,8 @@
                  (:cells proto-board)))))
 
 (defn remove-candidate [number cell-set]
+  "Given a number and cell set, it returns the cell set with the
+   number removed from its candidates."
   (let [result (reduce (fn [update [cell-pos cell-cands]]
                          (into update (if (and (> (count cell-cands) 1)
                                                (contains? cell-cands number))
@@ -60,6 +78,9 @@
     result))
 
 (defn remove-final-numbers [board new-final-numbers]
+  "Given a board and a list of numbers, it returns a new board
+   removing the given numbers from the candidates (except when one of
+   those numbers is the only candidate)."
   (let [update
         (reduce (fn [acc-cells final-number]
                   (let [[pos one-element-set] final-number
@@ -90,6 +111,11 @@
       updated-board)))
 
 (defn create-board [proto-board]
+  "Returns a board given a proto-board. A proto-board is a map with
+   keys :block-height, :block-width and :cells. The first two are the
+   height and width of a block (ie. 3 in a normal sudoku) and the last
+   is a list of lists with the contents of the board (either nil or a
+   number)."
   (if (well-formed? proto-board)
     (let [dim (* (:block-height proto-board)
                  (:block-width  proto-board))
@@ -119,6 +145,12 @@
     (throw (IllegalArgumentException. "Inconsistent board cells!"))))
 
 (defn consistent-sets? [board set-function]
+  "Given a board and a function to extract cell sets off a board, this
+   function returns true if none of the cell sets returned by the
+   set-function has repeated final numbers. The set-function will be
+   called dim times, with the given board and all numbers from 0 to
+   dim-1, where dim is the board size as returned by the 'dim'
+   function."
   (every?
    (fn [n]
      (let [final-numbers (map first
@@ -130,17 +162,24 @@
    (range (dim board))))
 
 (defn consistent? [board]
+  "Returns true if the given board doesn't have any final numbers
+   repeated, row-wise, col-wise or block-wise."
   (and (consistent-sets? board board-row)
        (consistent-sets? board board-col)
        (consistent-sets? board board-block)))
 
 (defn board-update [board update]
+  "Given a board and a cell set, returns a new board with the cell set
+   applied as an update."
   (let [final-numbers (filter #(= (count (second %)) 1)
                               update)
         updated-board (merge board {:cells (merge (:cells board) update)})]
     (remove-final-numbers updated-board final-numbers)))
 
 (defn ^:export solved? [board]
+  "Returns true if the given board is solved (all cells contain final
+   numbers). Throws an exception if the board is inconsistent, ie. if
+   there are any repeated final numbers."
   (if (consistent? board)
     (let [number-unknowns (count (filter (fn [coord-cand]
                                            (> (count (second coord-cand)) 1))
@@ -149,6 +188,7 @@
     (throw (IllegalStateException. "Inconsistent board!"))))
 
 (defn ^:export print-board
+  "Returns an HTML representation of the given board."
   ([board]
      (print-board board {}))
   ([board options]
@@ -160,6 +200,8 @@
                      :rule (get options :rule)})))
 
 (defn ^:export export-board [board]
+  "Returns a proto-board of the given board. Useful to eg. print an
+   ASCII version of the board."
   (let [index-range (range (* (:block-height board)
                               (:block-width board)))]
     (merge board
@@ -174,6 +216,11 @@
                              index-range))})))
 
 (defn ^:export import-board [filename]
+  "Returns a new board read from the given filename. The format of the
+   file must be a first line with two space-separated numbers
+   indicating the width and height of the block, and the rest of the
+   lines must be a space-separated contents of each successive
+   row. Each cell contents must be either a number or an underscore."
   (let [lines (clojure.string/split-lines (slurp filename))
         dimesions-spec (map read-string
                             (clojure.string/split (first lines) #" "))
