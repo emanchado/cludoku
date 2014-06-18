229ca9bcae0af717b1815d02260122d7178656b6
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2014-06-18 23:38:32 +0200

    Add very rough and simple rule application
    Now the ClojureScript version of cludoku can solve entire sudokus!

--- src/cludoku/core.cljs
+++ src/cludoku/core.cljs
@@ -1,7 +1,8 @@
 (ns cludoku.core
-  (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]
-            [cludoku.board :refer [create-board export-board dim]]))
+  (:require [cludoku.board :refer [create-board update-board dim]]
+            [cludoku.solver :as solver]
+            [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]))
 
 (enable-console-print!)
 
@@ -29,7 +30,9 @@
            [ 8  nil  2  nil nil nil  9  nil nil]
            [nil nil nil nil nil nil nil  6  nil]
            [ 3  nil  4  nil  1  nil nil nil  2 ]]})
-(def app-state (atom {:board (create-board medium-imported-board)}))
+(def app-state (atom {:states [{:board (create-board medium-imported-board)
+                                :applied-rule nil}]
+                      :current-state 0}))
 
 (defn sudoku-cell-view [candidate-set owner]
   (reify
@@ -45,11 +48,13 @@
                                c))
                     candidate-set))))))
 
-(defn sudoku-board-view [board owner]
+(defn sudoku-board-view [app owner]
   (reify
     om/IRender
     (render [this]
-      (let [cell-map (:cells board)
+      (let [board (:board (nth (:states app)
+                               (:current-state app)))
+            cell-map (:cells board)
             board-range (range (dim board))]
         (apply dom/div #js {:className "sudoku-board"}
                (map (fn [row-n]
@@ -60,5 +65,36 @@
                                   board-range)))
                     board-range))))))
 
-(om/root sudoku-board-view (:board @app-state)
+(defn next-step [app]
+  (let [states (:states app)
+        current-board (:board (get states (:current-state app)))]
+    (loop [pending-rules solver/rules]
+      (let [{rule-name :name rule-function :function} (first pending-rules)
+            update (rule-function current-board)
+            updated-board (update-board current-board update)]
+        (if (not= current-board updated-board)
+          (update-in app [:states] #(conj % {:board updated-board
+                                             :applied-updated update
+                                             :applied-rule rule-name}))
+          (if (> (count pending-rules) 1)
+            (recur (rest pending-rules))
+            app))))))
+
+(defn sudoku-controls-view [app owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div nil
+               (dom/button #js {:onClick (fn [e]
+                                           (let [number-states (-> @app :states count)]
+                                             (om/transact! app next-step)
+                                             (when (> (-> @app :states count)
+                                                      number-states)
+                                               (om/transact! app :current-state inc))))}
+                           "Next step")))))
+
+(om/root sudoku-board-view app-state
   {:target (. js/document (getElementById "sudoku-board"))})
+
+(om/root sudoku-controls-view app-state
+  {:target (. js/document (getElementById "sudoku-controls"))})
